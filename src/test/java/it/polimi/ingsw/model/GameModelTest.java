16e2cfbff8eaf37ef914806e876090c3382f0512
jonardan <j.sciaky@libero.it>
2023-03-17 13:41:01 +0100

    added tests for the game model class

--- src/test/java/it/polimi/ingsw/model/GameModelTest.java
+++ src/test/java/it/polimi/ingsw/model/GameModelTest.java
@@ -1,4 +1,45 @@
 package it.polimi.ingsw.model;
+import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 public class GameModelTest {
+    @Test
+    public void addPlayerTest() {
+        int numPlayers = 4;
+        var game = new GameModel(numPlayers, 0, 0);
+        for (int i=0; i < numPlayers; i++) {
+            var player=new Player("nick_"+i,i);
+            game.addPlayer("nick_"+i,i);
+
+
+
+
+
+
+            if (!(player.getNickname().equals(game.getPlayers().get(i).getNickname())&&player.getPg()==game.getPlayers().get(i).getPg())){
+                assertTrue(false);
+           }
+
+          //  assertTrue(player.equals(game.getPlayers().get(i)) );
+
+
+        }
+    }
+    @Test
+    public void getOccupiedTest(){
+        var game = new GameModel(4, 0, 0);
+        var occupied= game.getOccupied();
+        var board= game.getAllCoordinates();
+        for(int i=0;i< board.size();i++){
+            if(!(game.getTile(board.get(i)).equals(Tile.NOTILE))){
+                assertTrue(occupied.contains(board.get(i)));
+            }
+
+        }
+
+
+    }
+
+
+
 }
