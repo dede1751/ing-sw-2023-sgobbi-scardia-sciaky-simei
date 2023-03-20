e23770c89018a3c80d6bd0c42cea95b38cc9e214
luca-simei <luca.simei@mail.polimi.it>
2023-03-20 19:50:29 +0100

    Implemented 5 common goals so far

--- src/main/java/it/polimi/ingsw/model/goals/common/TwoColumnDistinctGoal.java
+++ src/main/java/it/polimi/ingsw/model/goals/common/TwoColumnDistinctGoal.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.model.goals.common;
 
 import it.polimi.ingsw.model.Shelf;
+import it.polimi.ingsw.model.Tile;
+
+import java.util.ArrayList;
 
 public class TwoColumnDistinctGoal implements CommonGoalStrategy {
     
@@ -9,7 +12,23 @@ public class TwoColumnDistinctGoal implements CommonGoalStrategy {
     }
     
     public boolean checkShelf(Shelf shelf) {
-        return false;
+        int counter = 0;
+        for (int j = 0; j < 5; j++) {
+            int[] temp = new int[] {1, 1, 1, 1, 1, 1};
+            int sum = 0;
+            for (int i = 0; i < 6; i++) {
+                if (shelf.getTile(i, j) == Tile.CATS) temp[0]--;
+                if (shelf.getTile(i, j) == Tile.BOOKS) temp[1]--;
+                if (shelf.getTile(i, j) == Tile.GAMES) temp[2]--;
+                if (shelf.getTile(i, j) == Tile.FRAMES) temp[3]--;
+                if (shelf.getTile(i, j) == Tile.TROPHIES) temp[4]--;
+                if (shelf.getTile(i, j) == Tile.PLANTS) temp[5]--;
+            }
+            for (int i = 0; i < 6; i++) {
+                sum = sum + temp[i];
+            }
+            if (sum == 0) counter++;
+        }
+        return counter >= 2;
     }
-    
 }
--- src/main/java/it/polimi/ingsw/model/goals/common/TwoRowDistinctGoal.java
+++ src/main/java/it/polimi/ingsw/model/goals/common/TwoRowDistinctGoal.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model.goals.common;
 
 import it.polimi.ingsw.model.Shelf;
+import it.polimi.ingsw.model.Tile;
 
 public class TwoRowDistinctGoal implements CommonGoalStrategy {
     
@@ -9,7 +10,35 @@ public class TwoRowDistinctGoal implements CommonGoalStrategy {
     }
     
     public boolean checkShelf(Shelf shelf) {
-        return false;
+        int counter = 0;
+        for (int i = 0; i < 6; i++) {
+            int[] temp = new int[] {1, 1, 1, 1, 1, 1};
+            int sum = 0;
+            for (int j = 0; j < 5; j++) {
+                if (shelf.getTile(i, j) == Tile.CATS) {
+                    if (temp[0] == 1) temp[0]--;
+                }
+                if (shelf.getTile(i, j) == Tile.BOOKS) {
+                    if (temp[1] == 1) temp[1]--;
+                }
+                if (shelf.getTile(i, j) == Tile.GAMES) {
+                    if (temp[2] == 1) temp[2]--;
+                }
+                if (shelf.getTile(i, j) == Tile.FRAMES) {
+                    if (temp[3] == 1) temp[3]--;
+                }
+                if (shelf.getTile(i, j) == Tile.TROPHIES) {
+                    if (temp[4] == 1) temp[4]--;
+                }
+                if (shelf.getTile(i, j) == Tile.PLANTS) {
+                    if (temp[5] == 1) temp[5]--;
+                }
+            }
+            for (int j = 0; j < 6; j++) {
+                sum = sum + temp[i];
+            }
+            if (sum == 1) counter++;
+        }
+        return counter >= 2;
     }
-    
 }
