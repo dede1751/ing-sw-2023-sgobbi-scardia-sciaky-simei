958bdac8f6bc20eecc88ef8c34846feff0768c92
luca-simei <luca.simei@mail.polimi.it>
2023-03-21 12:50:17 +0100

    First common goal test implemented

--- src/test/java/it/polimi/ingsw/model/goals/CommonGoalTest.java
+++ src/test/java/it/polimi/ingsw/model/goals/CommonGoalTest.java
@@ -1,4 +1,30 @@
 package it.polimi.ingsw.model.goals;
 
+import it.polimi.ingsw.model.Shelf;
+
+import it.polimi.ingsw.model.Tile;
+import it.polimi.ingsw.model.goals.common.CommonGoal;
+import it.polimi.ingsw.model.goals.common.CrossGoal;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This class tests {@link CommonGoal}
+ */
+
 public class CommonGoalTest {
+    
+    @Test
+    public void CrossGoalTest() {
+        var shelf = new Shelf();
+        shelf.addTiles(List.of(Tile.CATS, Tile.TROPHIES, Tile.CATS), 0);
+        shelf.addTiles(List.of(Tile.PLANTS, Tile.CATS, Tile.PLANTS), 0);
+        shelf.addTiles(List.of(Tile.CATS, Tile.TROPHIES, Tile.CATS), 0);
+        var goal = new CrossGoal();
+        assertTrue(goal.checkShelf(shelf));
+    }
 }
+
