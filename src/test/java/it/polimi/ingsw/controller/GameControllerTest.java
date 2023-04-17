b3452d0ee1b526e7d4afefcf2b02969ca4004ac9
luca-simei <luca.simei@mail.polimi.it>
2023-04-17 15:13:50 +0200

    New test and minor fixes

--- src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
@@ -4,6 +4,7 @@ import com.google.gson.*;
 import it.polimi.ingsw.model.GameModel;
 import it.polimi.ingsw.model.Tile;
 import it.polimi.ingsw.utils.files.ResourcesManager;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -19,45 +20,88 @@ import static org.junit.jupiter.api.Assertions.*;
 @Tag("Controller")
 class GameControllerTest {
     
-    private String getResource(String className) {
-        String result = null;
-        try {
-            result = Files.readString(
-                    Path.of(ResourcesManager.testRootDir,
-                            "it/polimi/ingsw/controller/resources/" + className + "Test.json"),
-                    StandardCharsets.UTF_8);
-        }
-        catch( IOException e ) {
-            e.printStackTrace(System.err);
-        }
-        return result;
+    private String getResource(String className) throws IOException {
+        return Files.readString(
+                Path.of(ResourcesManager.testRootDir,
+                        "it/polimi/ingsw/controller/resources/" + className + "Test.json"),
+                StandardCharsets.UTF_8);
     }
     
     @Test
     public void needRefillTrue() {
         var name = ResourcesManager.getCurrentMethod();
-        var json = getResource(name);
-        Gson gson = new GsonBuilder().registerTypeAdapter(GameModel.class, new GameModel.ModelDeserializer()).create();
-        var model = gson.fromJson(json, GameModel.class);
-        var controller = new GameController(model, new ArrayList<>());
-        assertTrue(controller.needRefill());
+        String json;
+        try {
+            json = getResource(name);
+            Gson gson =
+                    new GsonBuilder().registerTypeAdapter(GameModel.class, new GameModel.ModelDeserializer()).create();
+            var model = gson.fromJson(json, GameModel.class);
+            var controller = new GameController(model, new ArrayList<>());
+            assertTrue(controller.needRefill());
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+            fail();
+        }
+        
     }
     
-    @Test
-    public void turnManagerRefillTrue() {
-        var name = ResourcesManager.getCurrentMethod();
-        var json = getResource(name);
-        Gson gson = new GsonBuilder().registerTypeAdapter(GameModel.class, new GameModel.ModelDeserializer()).create();
-        var model = gson.fromJson(json, GameModel.class);
-        var controller = new GameController(model, new ArrayList<>());
-        assertTrue(controller.needRefill());
-        var tiles = model.getBoard().getTiles();
-        var numofNoTiles = Collections.frequency(tiles.values()
-                                                           .stream()
-                                                           .map(Tile::type)
-                                                           .toList(), Tile.Type.NOTILE);
-        controller.turnManager();
-        var numofTiles = tiles.values().stream().filter((tile) -> tile != Tile.NOTILE).count();
-        assertEquals( numofNoTiles, numofTiles );
+    @Tag("turnManager")
+    @Nested
+    class turnManagerTest {
+        
+        @Test
+        public void turnManagerRefillTrue1() {
+            var name = ResourcesManager.getCurrentMethod();
+            String json;
+            
+            try {
+                json = getResource(name);
+                Gson gson =
+                        new GsonBuilder().registerTypeAdapter(GameModel.class, new GameModel.ModelDeserializer()).create();
+                var model = gson.fromJson(json, GameModel.class);
+                var controller = new GameController(model, new ArrayList<>());
+                assertTrue(controller.needRefill());
+                var tiles = model.getBoard().getTiles();
+                var numofNoTiles = Collections.frequency(tiles.values()
+                                                                 .stream()
+                                                                 .map(Tile::type)
+                                                                 .toList(), Tile.Type.NOTILE);
+                controller.turnManager();
+                tiles = model.getBoard().getTiles();
+                var numofTiles = tiles.values().stream().filter((tile) -> tile != Tile.NOTILE).count();
+                assertEquals(numofNoTiles, numofTiles);
+            }
+            catch( IOException e ) {
+                e.printStackTrace();
+                fail();
+            }
+        }
+        
+        @Test
+        public void turnManagerRefillTrue2() {
+            var name = ResourcesManager.getCurrentMethod();
+            String json;
+            try {
+                json = getResource(name);
+                Gson gson =
+                        new GsonBuilder().registerTypeAdapter(GameModel.class, new GameModel.ModelDeserializer()).create();
+                var model = gson.fromJson(json, GameModel.class);
+                var controller = new GameController(model, new ArrayList<>());
+                assertTrue(controller.needRefill());
+                controller.turnManager();
+                var tiles = model.getBoard().getTiles();
+                var tilesOnBoard = tiles.values()
+                                              .stream()
+                                              .filter((tile) -> !tile.equals(Tile.NOTILE))
+                                              .count();
+                var tilesInTileBag = model.getTileBag().currentTileNumber();
+                assertEquals(tilesInTileBag, tilesOnBoard);
+            }
+            catch( IOException e ) {
+                e.printStackTrace();
+                fail();
+            }
+        }
     }
 }
