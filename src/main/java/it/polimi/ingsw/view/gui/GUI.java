355285dc773cf7236e3442ba96e84ec23a96a4ab
Roberto Scardia <roberto.scardia@gmail.com>
2023-04-30 19:07:24 +0000

    added uml

--- src/main/java/it/polimi/ingsw/view/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/view/gui/GUI.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.view.gui;
 
-import it.polimi.ingsw.model.messages.ModelMessage;
+import it.polimi.ingsw.model.messages.*;
 import it.polimi.ingsw.view.View;
 
 
@@ -9,6 +9,26 @@ public class GUI extends View {
     @Override
     public void update(ModelMessage<?> msg){
         //TODO
+    }
+    
+    @Override
+    public void onMessage(BoardMessage msg) {
+    
+    }
+    
+    @Override
+    public void onMessage(AvailableLobbyMessage msg) {
+    
+    }
+    
+    @Override
+    public void onMessage(EndGameMessage msg) {
+    
+    }
+    
+    @Override
+    public void onMessage(StartGameMessage msg) {
+    
     }
     
     @Override
