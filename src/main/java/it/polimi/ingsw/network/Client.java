975fa556965e78574d80ce8e9603706797f3451b
Roberto Scardia <roberto.scardia@gmail.com>
2023-05-01 01:23:38 +0000

    added Client class documentation

--- src/main/java/it/polimi/ingsw/network/Client.java
+++ src/main/java/it/polimi/ingsw/network/Client.java
@@ -7,13 +7,22 @@ import java.rmi.RemoteException;
 
 public interface Client extends Remote {
     
+    /**
+     * Sets the internal client ID. Should only be called once inside a Server thread.
+     * The ID is unique and is used by the server to identify each client.
+     * @param clientID The ID number assigned by the server
+     */
     void setClientID(int clientID) throws RemoteException;
     
+    
+    /**
+     * Returns the ID client
+     */
     int getClientID() throws RemoteException;
+    
     /**
      * Notify the client of a model change
      * @param msg   The object that contains the model changes information
      */
     void update(ModelMessage<?> msg) throws RemoteException;
-    
 }
