a3edfec2bb0161bf0c0cc476d908b8f43b26d29f
Roberto Scardia <roberto.scardia@gmail.com>
2023-05-02 02:18:46 +0000

    fixed UpdateScoreMessages

--- src/main/java/it/polimi/ingsw/model/messages/UpdateScoreMessage.java
+++ src/main/java/it/polimi/ingsw/model/messages/UpdateScoreMessage.java
@@ -1,19 +1,8 @@
 package it.polimi.ingsw.model.messages;
 
-public class UpdateScoreMessage extends ModelMessage<Integer> {
+public class UpdateScoreMessage extends ModelMessage<UpdateScorePayload> {
     
-    public enum Type {
-        CommonGoal, PersonalGoal, Adiajency, Bonus
-    }
-    
-    private final Type type;
-    
-    public UpdateScoreMessage(Integer score, Type type) {
-        super(score);
-        this.type = type;
-    }
-    
-    public Type getType() {
-        return this.type;
+    public UpdateScoreMessage(Integer score, UpdateScorePayload.Type type, String nickname) {
+        super(new UpdateScorePayload(type, score, nickname));
     }
 }
