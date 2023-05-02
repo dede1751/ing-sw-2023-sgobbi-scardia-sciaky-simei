576d483a542f217987434da5bb2a73610f88ca7f
Roberto Scardia <roberto.scardia@gmail.com>
2023-05-02 01:37:52 +0000

    fixed StartGameMessage

--- src/main/java/it/polimi/ingsw/model/messages/StartGameMessage.java
+++ src/main/java/it/polimi/ingsw/model/messages/StartGameMessage.java
@@ -2,27 +2,9 @@ package it.polimi.ingsw.model.messages;
 
 import java.util.List;
 
-public class StartGameMessage extends ModelMessage<StartGamePayload>{
+public class StartGameMessage extends ModelMessage<StartGamePayload> {
     
-    
-    private final int personalGoalId;
-    private final int XCGnumber;
-    private final int YCGnumber;
     public StartGameMessage(List<String> p, Integer personalGoalId, Integer XCGnumber, Integer YCGnumber) {
-        super(new StartGamePayload(p));
-        this.personalGoalId = personalGoalId;
-        this.XCGnumber=XCGnumber;
-        this.YCGnumber=YCGnumber;
-    }
-    public int getPersonalGoalId(){
-        return this.personalGoalId;
-    }
-    
-    public int getYCGnumber() {
-        return YCGnumber;
-    }
-    
-    public int getXCGnumber() {
-        return XCGnumber;
+        super(new StartGamePayload(p, personalGoalId, XCGnumber, YCGnumber));
     }
 }
--- src/main/java/it/polimi/ingsw/model/messages/StartGamePayload.java
+++ src/main/java/it/polimi/ingsw/model/messages/StartGamePayload.java
@@ -3,5 +3,4 @@ package it.polimi.ingsw.model.messages;
 import java.io.Serializable;
 import java.util.List;
 
-public record StartGamePayload(List<String> nicknames) implements Serializable {
-}
+public record StartGamePayload(List<String> nicknames, int personalGoalId, int XCGnumber, int YCGnumber) implements Serializable {}
