0b787d861d5b2903a4e4dd0e2e4eab253369a4e2
jonatan Sciaky <j.sciaky@libero.it>
2023-05-20 15:48:58 +0200

    started the fxml file with a couple of buttons and some assets

--- src/main/java/it/polimi/ingsw/view/gui/GUIApp.java
+++ src/main/java/it/polimi/ingsw/view/gui/GUIApp.java
@@ -1,7 +1,10 @@
 package it.polimi.ingsw.view.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class GUIApp extends Application {
     
@@ -9,11 +12,14 @@ public class GUIApp extends Application {
     private GUI guiContext;
     
     /**
-     *
      * @param guiContext
      */
-    public GUIApp (GUI guiContext){
+    public GUIApp(GUI guiContext) {
         this.guiContext = guiContext;
+    }
+    
+    public GUIApp() {
+    
     }
     
     /**
@@ -23,10 +29,11 @@ public class GUIApp extends Application {
      */
     @Override
     public void start(Stage stage) throws Exception {
-    
+        Parent root = FXMLLoader.load(getClass().getResource("/myshelfie.fxml"));
+        stage.setTitle("My Shelfie");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
-    
-    
     
     
 }
